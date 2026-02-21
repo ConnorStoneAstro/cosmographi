149b94eb5a6c51835eb03ce8f6402fbf4ca3a757
Connor Stone <connorstone628@gmail.com>
2026-02-20 19:20:04 -0500

    survey crossmatch now works

--- src/cosmographi/utils/survey.py
+++ src/cosmographi/utils/survey.py
@@ -1,3 +1,16 @@
+from scipy.spatial import KDTree
+import numpy as np
+
+
+def radec_to_sphere(ra, dec):
+    ra_rad = np.radians(ra + 180)
+    dec_rad = np.radians(dec + 90)
+    x = np.sin(dec_rad) * np.cos(ra_rad)
+    y = np.sin(dec_rad) * np.sin(ra_rad)
+    z = np.cos(dec_rad)
+    return np.stack((x, y, z), axis=1)  # (N, 3)
+
+
 def cross_match_survey_circle(
     source_tmin,
     source_tmax,
@@ -50,8 +63,56 @@ def cross_match_survey_circle(
 
     Returns
     -------
-    match_indices : list[list]
-        Matches between sources and survey images. List with entry for every
-        source (same order), with elements that are lists of the indices of
-        matching survey observations.
+    match_indices : dict[list]
+        Matches between sources and survey images. Dict with entry for every
+        source that has oen or more matches (identified by index in original
+        array), with elements that are lists of the indices of matching survey
+        observations.
     """
+
+    Nsrc = np.arange(len(source_tmin))
+    Nsrv = np.arange(len(survey_t))
+
+    coord_src = radec_to_sphere(source_ra, source_dec)
+    coord_srv = radec_to_sphere(survey_ra, survey_dec)
+    # Get distance between two points separated by the fov radius
+    coord_r = np.linalg.norm(
+        np.diff(radec_to_sphere(np.zeros(2), np.array([0, survey_fov / 2])), axis=0)
+    )
+
+    t_range = np.max(survey_t) - np.min(survey_t)
+    t_chunks = np.linspace(
+        np.min(survey_t), np.max(survey_t) + chunk_time / 100, int(np.ceil(t_range / chunk_time))
+    )
+
+    all_matches = {}
+    for tstart, tend in zip(t_chunks[:-1], t_chunks[1:]):
+        sel_src = (source_tmax >= tstart) & (source_tmin < tend)  # src with any overlap in window
+        sel_srv = (survey_t >= tstart) & (survey_t < tend)  # imgs in window
+
+        tree_src = KDTree(coord_src[sel_src])
+        tree_srv = KDTree(coord_srv[sel_srv])
+        matches = tree_src.query_ball_tree(tree_srv, coord_r)
+
+        Nsrc_chunk = Nsrc[sel_src]
+        Nsrv_chunk = Nsrv[sel_srv]
+
+        src_tmin_chunk = source_tmin[sel_src]
+        src_tmax_chunk = source_tmax[sel_src]
+        srv_t_chunk = survey_t[sel_srv]
+
+        for i, m in enumerate(matches):
+            if len(m) == 0:
+                continue
+            # Further refine selection by exact time, rather than chunk membership
+            m = np.array(m, dtype=int)
+            m = m[(srv_t_chunk[m] > src_tmin_chunk[i]) & (srv_t_chunk[m] < src_tmax_chunk[i])]
+            if len(m) == 0:
+                continue
+            isrc = int(Nsrc_chunk[i])
+            if isrc in all_matches:
+                all_matches[isrc] = np.append(all_matches[isrc], Nsrv_chunk[m])
+            else:
+                all_matches[isrc] = Nsrv_chunk[m]
+
+    return all_matches
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,10 @@
 import jax.numpy as jnp
 import jax
 import pytest
+import numpy as np
+from astropy.coordinates import angular_separation
+from astropy import units as u
+from time import process_time
 
 import cosmographi as cg
 
@@ -71,3 +75,53 @@ def test_int_Phi_N(mu1, mu2):
     res_quad = cg.utils.quad(lambda x: Phi(x) * N(x), -10, 10, 1000)
     res_int = cg.utils.int_Phi_N(mu1, sigma1, mu2, sigma2)
     assert jnp.isclose(res_quad, jnp.exp(res_int), rtol=1e-8)
+
+
+def test_survey_cross_match():
+    np.random.seed(42)
+    Nsrc = 1_000
+    Nsrv = 2_000_000
+    survey_ra = np.random.uniform(-180, 180, Nsrv)
+    survey_dec = np.rad2deg(np.arcsin(2 * np.random.uniform(size=Nsrv) - 1))
+
+    survey_t = np.linspace(0, 365 * 10, Nsrv)
+
+    sel = np.random.choice(len(survey_ra))
+    source_ra = survey_ra[sel] + np.random.normal(
+        scale=20 / np.cos(np.deg2rad(survey_dec[sel])), size=Nsrc
+    )
+    source_dec = survey_dec[sel] + np.random.normal(scale=20, size=Nsrc)
+    source_t = np.random.uniform(0, 365 * 10, Nsrc)
+    source_tmin = source_t - 20
+    source_tmax = source_t + 60
+
+    start = process_time()
+    matches = cg.utils.cross_match_survey_circle(
+        source_tmin,
+        source_tmax,
+        source_ra,
+        source_dec,
+        survey_t,
+        survey_ra,
+        survey_dec,
+        survey_fov=2 * 1.75,
+    )
+    print("runtime: ", process_time() - start)
+    # print(matches)
+    assert len(matches) > 5, "Some SN should match"
+    for key in list(matches.keys()):
+        assert source_tmin[key] < survey_t[matches[key][0]], (
+            "matched image should be in time window"
+        )
+        assert survey_t[matches[key][0]] < source_tmax[key], (
+            "matched image should be in time window"
+        )
+        assert (
+            angular_separation(
+                source_ra[key] * u.deg,
+                source_dec[key] * u.deg,
+                survey_ra[matches[key][0]] * u.deg,
+                survey_dec[matches[key][0]] * u.deg,
+            )
+            <= 1.75 * u.deg
+        ), "matched image should contain SN in fov"
